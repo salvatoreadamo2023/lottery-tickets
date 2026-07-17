c5bc29c87ab80cbdac2960ac0f6781b51add219a
LAPTOP-LA0VEM1P\salva <salvatore.adamo@cgi.com>
2026-07-17 22:48:13 +0200

    first commit Spring boot app lottery backend

---
+++ src/main/java/com/lottery/tickets/LotteryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lottery.tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lottery/tickets/LotteryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lottery.tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
